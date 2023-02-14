7bbc8c0d074fe9fb000c9cb10071680d8b0239da
jtw007 <joshwu095@gmail.com>
2023-02-14 12:32:01 -0800

    updated read me and render on search

--- src/components/pages/Search.jsx
+++ src/components/pages/Search.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
 import { useNavigate, Navigate } from 'react-router-dom'
 import jwtDecode from 'jwt-decode'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Search = () => {
     const [ search, setSearch ] = useState('')
+    const [fit, setFit] = useState()
     
     const handleChange = (e) => {
         e.preventDefault()
@@ -15,6 +17,30 @@ const Search = () => {
         query: '',
         list: []
     })
+    
+    useEffect(() => {
+		const fetchData = async () => {
+				try {
+					// get the token from local storage
+					const token = localStorage.getItem('jwt')
+					// hit the auth locked endpoint
+					const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/inventory`, {headers: {'Authorization': token}})
+                    setFit(response.data)
+                    // this console logs all dbs clothes
+                    console.log(response.data)
+				} catch (err) {
+					// if the error is a 401 -- that means that auth failed
+					console.warn(err)
+					if (err.response) {
+						if (err.response.status === 401) {
+							// send the user to the login screen
+							Navigate('/login')
+						}
+					}
+				}
+			}
+			fetchData()
+	}, [])
 
     //localStorage = web storage object that allows JS sites and apps to keep key-value pairs in web browser with no expiration date; enables developers to store and retrieve data in the browser - not good practice since data will be lost if the user clears cache
         //in this case, we are storing the jwt 
@@ -25,6 +51,15 @@ const Search = () => {
         }
         const decoded = jwtDecode(token)
         console.log(decoded.id)
+
+        const fitComponents = fit?.map((fit, idx) => {
+            return(
+                <div key={`fit-${idx}`}>
+                    <h2>{fit.nickname}</h2>
+                    <p>{fit.type}</p>
+                </div>
+            )
+        })
 
     return ( 
         
@@ -51,7 +86,7 @@ const Search = () => {
                 </div>
             </form>
             
-            
+            {fitComponents}
         </div>
         
      );
