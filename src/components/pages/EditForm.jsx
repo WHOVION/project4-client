407c35e47eec0c9bb630400f49cb70a0be8217b3
ericcnguyen04 <ericc.win@gmail.com>
2023-02-15 13:54:13 -0500

    redid editform

--- src/components/pages/EditForm.jsx
+++ src/components/pages/EditForm.jsx
@@ -1,20 +1,29 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-const EditForm = () => {
-    return ( 
-        <div>
-            <form onSubmit={handleEdit} />
-                <input 
-                    type="text"
-                    id="nickname"
-                    placeholder='nickname...'
-                    value={form.nickname} 
-                    onChange={e => setForm({...form, nickname: e.target.value})}
-                />
-        </div>
-     );
+function EditForm() {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 }
- 
-export default EditForm;
 
-// have a useState for for,setForm
-// initial state going to be the prop for that fit
+export default EditForm;
