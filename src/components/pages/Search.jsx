7aae91f5108cc713ada75925d650cb5b13eaa3ee
ericcnguyen04 <ericc.win@gmail.com>
2023-02-16 20:12:39 -0500

    one last time v2

--- src/components/pages/Search.jsx
+++ src/components/pages/Search.jsx
@@ -81,35 +81,6 @@ const handleDeleteClick = async (idx) => {
   return (
     <div className="search-container">
       <center>
-<<<<<<< HEAD
-      <div className="search-title">
-        <h1>Search Closet</h1>
-      </div>
-
-      <input
-        type="text"
-        onChange={(e) => {
-          setFilter({ ...filter, nickname: e.target.value })
-        }}
-        value={filter.nickname}
-      />
-      <select
-        onChange={(e) => {
-          setFilter({ ...filter, type: e.target.value })
-        }}
-        value={filter.type}
-      >
-        {options.map((option) => {
-          return (
-            <option key={option} value={option}>
-              {option}
-            </option>
-          )
-        })}
-      </select>
-      <button onClick={handleReset}>Reset</button>
-      </center>
-=======
         <div className="search-title">
           <h1>Search Closet</h1>
         </div>
@@ -138,7 +109,6 @@ const handleDeleteClick = async (idx) => {
         <button onClick={handleReset}>Reset</button>
       </center>
 
->>>>>>> main
       <div className='cardholder'>
         {fitComponents}
       </div>
